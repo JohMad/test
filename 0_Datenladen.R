42e4da0dbde95b2c6f04a694fdb7f2b3d3d2c354
JM <j.maderspacher@gmx.net>
2018-02-07 10:50:00 +0100

    Update zustäzlicher ggplot

--- 0_Datenladen.R
+++ 0_Datenladen.R
@@ -9,3 +9,7 @@ daten <- read_csv("data.csv")
 head(daten)
 
 plot(daten$UnitPrice[1:100])
+
+ggplot(daten[1:100,],aes(x  = StockCode,y = UnitPrice)) + geom_point()
+
+ggsave("UnitPrice.png",width=7,height=5)

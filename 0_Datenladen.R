1eee3d26feed308de4d0309a773fbe0f3a9e351a
JM <j.maderspacher@gmx.net>
2018-02-07 10:41:54 +0100

    Update laden der Daten

--- 0_Datenladen.R
+++ 0_Datenladen.R
@@ -3,6 +3,7 @@ library(tidyverse)
 
 # Daten Laden -------------------------------------------------------------
 
-setwd("C:/Users/JM/Desktop/Data_Mining/Clustering_Costumer/")
+setwd("C:/Users/JM/Desktop/Data_Mining/git_test/")
 daten <- read_csv("data.csv")
 
+head(daten)

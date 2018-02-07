e89da4c8cc76fb51142b792638060f60e95a0540
JM <j.maderspacher@gmx.net>
2018-02-07 10:32:37 +0100

    Script zum laden der Daten

---
+++ 0_Datenladen.R
@@ -0,0 +1,8 @@
+
+library(tidyverse)
+
+# Daten Laden -------------------------------------------------------------
+
+setwd("C:/Users/JM/Desktop/Data_Mining/Clustering_Costumer/")
+daten <- read_csv("data.csv")
+

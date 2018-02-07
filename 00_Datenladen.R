4643db5d033765499e987a368a153570ed9c0832
JM <j.maderspacher@gmx.net>
2018-02-07 10:57:19 +0100

    Vorversion Datenanalyse

---
+++ 00_Datenladen.R
@@ -0,0 +1,11 @@
+
+library(tidyverse)
+
+# Daten Laden -------------------------------------------------------------
+
+setwd("C:/Users/JM/Desktop/Data_Mining/git_test/")
+daten <- read_csv("data.csv")
+
+head(daten)
+
+plot(daten$UnitPrice[1:100])
